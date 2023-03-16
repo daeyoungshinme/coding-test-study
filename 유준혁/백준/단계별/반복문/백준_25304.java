3332e909182e53dcc7208cbba9e5d24193e9bdbf
Jimin <thswlals0219@gmail.com>
2023-03-16 14:01:29 +0900

    Revert "Merge branch 'frost0807:main' into jimin"
    This reverts commit 57909c2d3ff6dfa0d37486536a37838353b9b0af, reversing
    changes made to c7b5b6bff0f7484e7d4ecd69446ce4a8f6bc52d0.

--- 유준혁/백준/단계별/반복문/백준_25304.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt(), n = sc.nextInt();
-		while(n--> 0) x -= sc.nextInt() * sc.nextInt();
-		System.out.print(x == 0 ? "Yes" : "No");
-	}
-}
--- 유준혁/백준/단계별/반복문/백준_25314.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt() / 4;
-		while(n--> 0) System.out.print("long ");
-		System.out.print("int");
-	}
-}
